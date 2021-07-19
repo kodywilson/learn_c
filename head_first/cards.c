fc17153dd7ee19298776fc2529c6d30187721b89
Kody Wilson <kodywilson@gmail.com>
2021-07-19 13:35:42 -0700

    initial commit of in_easy_steps

--- head_first/cards.c
+++ head_first/cards.c
@@ -5,26 +5,40 @@
 int main()
 {
   char card_name[3];
-  puts("Enter the card name: ");
-  scanf("%2s", card_name);
-  int val = 0;
-  if (card_name[0] == 'K') {
-    val = 10;
-  } else if (card_name[0] == 'Q') {
-    val = 10;
-  } else if (card_name[0] == 'J') {
-    val = 10;
-  } else if (card_name[0] == 'A') {
-    val = 11;
-  } else {
-    val = atoi(card_name);
+  int count = 0;
+  while (card_name[0] != 'X')
+  {
+    puts("Enter the card name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    switch(card_name[0]) {
+      case 'K':
+      case 'Q':
+      case 'J':
+        val = 10;
+        break;
+      case 'A':
+        val = 11;
+        break;
+      case 'X':
+        puts("Thanks for playing!");
+        continue;
+      default:
+        val = atoi(card_name);
+        if (val < 1 || val > 11) {
+          puts("Please enter a valid card value");
+          continue;
+        }
+    }
+    printf("The card value is: %i\n", val);
+    // Check if the value is 3 to 6
+    if ( val > 2 && val < 7 ) {
+      count++;
+    } else if ( val == 10 ) {
+      count--;
+    }
+    printf("Current count: %i\n", count);
   }
-  printf("The card value is: %i\n", val);
-  // Check if the value is 3 to 6
-  if ( val >= 3 && val <= 6 )
-    puts("Count has gone up");
-  else if ( val < 3 || val > 6 )
-    puts("Count has gone down");
-  return 0;
+    return 0;
 }
 
