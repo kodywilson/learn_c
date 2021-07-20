c8f039556ccb798502943d09658b421717a17dbf
Kody Wilson <kodywilson@gmail.com>
2021-07-19 18:15:59 -0700

    two dimensional array

---
+++ in_easy_steps/matrix.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int matrix[2][3] = { {'A','B','C'},{1,2,3} };
+  printf("Element [0][0] contains %c\n", matrix[0][0]);
+  printf("Element [0][1] contains %c\n", matrix[0][1]);
+  printf("Element [0][2] contains %c\n", matrix[0][2]);
+  printf("Element [1][0] contains %c\n", matrix[1][0]);
+  printf("Element [1][1] contains %c\n", matrix[1][1]);
+  printf("Element [1][2] contains %c\n", matrix[1][2]);
+  return 0;
+}
+
