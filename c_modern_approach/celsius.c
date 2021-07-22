a23c46cb65c34f92084001810fdae38746865f1d
Kody Wilson <kodywilson@gmail.com>
2021-07-22 16:56:50 -0700

    Calculate Celsius from Fahrenheit

---
+++ c_modern_approach/celsius.c
@@ -0,0 +1,16 @@
+// Convert Fahrenheit to Celsius
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void)
+{
+  float fahrenheit, celsius;
+  printf("Enter Fahrenheit temperature: ");
+  scanf("%f", &fahrenheit);
+  celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+  printf("%.2f Fahrenheit is equal to %.2f in Celsius.\n", fahrenheit, celsius);
+  return 0;
+}
+
