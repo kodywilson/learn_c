47e9315d5ec0c55020d5e507826a7f2ab2ed0339
Kody Wilson <kodywilson@gmail.com>
2021-07-23 15:12:54 -0700

    take input for sphere volumes

--- c_modern_approach/ch2/projects/sphere.c
+++ c_modern_approach/ch2/projects/sphere.c
@@ -6,7 +6,9 @@
 
 int main(void)
 {
-  int radius = 10;
+  float radius = 10.0f;
+  printf("Enter radius of sphere: "); 
+  scanf("%f", &radius); 
   printf("Volume of sphere is %.2f\n", VOL_RATIO * PI * radius * radius * radius);
   return 0;
 }
