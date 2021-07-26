1c94042452fa1326ef2ab1e1682b10cb1ea4c9bf
Kody Wilson <kodywilson@gmail.com>
2021-07-25 20:30:53 -0700

    finish up tprintf

--- c_modern_approach/ch3/tprintf.c
+++ c_modern_approach/ch3/tprintf.c
@@ -9,7 +9,7 @@ int main(void)
 
   i = 40;
   x = 839.21f;
-  printf("|%d|%5d|%-5d|\n", i , i, i);
+  printf("|%d|%5d|%-5d|%5.3d|\n", i , i, i, i);
   printf("|%10.3f|%10.3e|%-10g|\n", x , x, x);
   return 0;
 }
