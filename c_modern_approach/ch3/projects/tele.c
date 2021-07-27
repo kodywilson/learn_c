70c9050a40be97b61539e3aca9e76ab225646b12
Kody Wilson <kodywilson@gmail.com>
2021-07-26 17:53:03 -0700

    ch3 project 4

---
+++ c_modern_approach/ch3/projects/tele.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int grp1, grp2, grp3;
+  printf("Enter a phone number [(xxx) xxx-xxxx]: ");
+  scanf("(%d) %d-%d", &grp1, &grp2, &grp3);
+  printf("You entered %d.%d.%d\n", grp1, grp2, grp3);
+  return 0;
+}
+
