3769d6e292e39c74e55ba07d16d3f2c1ba876168
Kody Wilson <kodywilson@gmail.com>
2021-07-28 20:42:03 -0700

    ch4 project 6 ean codes

---
+++ c_modern_approach/ch4/projects/ean.c
@@ -0,0 +1,24 @@
+// This program calculates the check digit for EAN codes
+
+#include <stdio.h>
+
+int main()
+{
+  // Variables - check, first, sum1, sum2, and manufacturer group of 5 digits
+  int check, first, sum1, sum2, x1, x2, x3, x4, x5, x6,
+      y1, y2, y3, y4, y5; // item code group of 5
+
+  // Take input from user
+  printf("Enter the first 13 digits of an EAN: ");
+  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &first, &x1, &x2, &x3, &x4, &x5, &x6, &y1, &y2, &y3, &y4, &y5);
+
+  // Calculate check digit
+  sum1 = x1 + x3 + x5 + y1 + y3 + y5;
+  sum2 = first + x2 + x4 + x6 + y2 + y4;
+  check = 9 - ((sum1 * 3 + sum2 - 1) % 10);
+
+  // Display result
+  printf("Check digit: %d\n", check);
+  return 0;
+}
+
