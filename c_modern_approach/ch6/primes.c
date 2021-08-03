98bfb990457f4d6dcc69b107df9cd3728beb0b9d
Kody Wilson <kodywilson@gmail.com>
2021-08-02 20:32:11 -0700

    count primes up to n

--- c_modern_approach/ch6/primes.c
+++ c_modern_approach/ch6/primes.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-  int i, x;
+  int count = 0, i, x;
 
   // Get number from user
   printf("Enter a number: ");
   scanf("%d", &x);
   
-  for (int j = 1; j <= x; j++)
+  for (int j = 2; j <= x; j++)
   {
     for (i = 2; i < j && j % i != 0; i++);
     if (i < j)
@@ -18,8 +18,11 @@ int main(void)
     else
     {
       printf("%d is prime\n", j);
+      count++;
     }
   }
+
+  printf("\nThere are %d prime numbers from 1 to %d\n", count, x);
 
   return 0;
 
