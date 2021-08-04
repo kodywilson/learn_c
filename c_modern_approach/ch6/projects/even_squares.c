9796e9ff043e8cf9548449e5a55959836a2eda0e
Kody Wilson <kodywilson@gmail.com>
2021-08-04 14:24:27 -0700

    streamline even_squares program

--- c_modern_approach/ch6/projects/even_squares.c
+++ c_modern_approach/ch6/projects/even_squares.c
@@ -2,16 +2,14 @@
 
 int main(void)
 {
-  int i, n, square;
+  int i, n;
 
   printf("Enter a number: ");
   scanf("%d", &n);
 
   for (i = 2; i * i <= n; i+=2)
   {
-    square = i * i;
-    if (square % 2 == 0)
-      printf("%d\n", square);
+    printf("%d\n", i * i);
   }
 
   return 0;
