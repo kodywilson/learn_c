a4540e3a1deb481ed91c22d6ad7fcbe6fd7024d8
Kody Wilson <kodywilson@gmail.com>
2021-08-05 10:53:44 -0700

    ch7 prj6 sizeof program

---
+++ c_modern_approach/ch7/projects/sizeof.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("%ld %ld %ld %ld %ld %ld\n", sizeof(int), sizeof(short), sizeof(long), sizeof(float), sizeof(double), sizeof(long double));
+  return 0;
+}
+
