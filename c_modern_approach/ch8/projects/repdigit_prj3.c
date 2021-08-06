a9c90a6f7f3058ef9af1c702e0b02ae0dc3e980b
Kody Wilson <kodywilson@gmail.com>
2021-08-06 14:09:54 -0700

    ch8 prj3 looping repdigit

---
+++ c_modern_approach/ch8/projects/repdigit_prj3.c
@@ -0,0 +1,36 @@
+// find numbers with duplicate digits
+#include <stdio.h>
+
+#define N 10
+
+int main(void)
+{
+  int digit_seen[N] = {0};
+  int digit;
+  long n, x;
+
+  for (;;)
+  {
+    printf("Enter a number: ");
+    scanf("%ld", &x);
+    n = x;
+    if (x < 1)
+      break;
+    while (n > 0)
+    {
+      digit = n % 10;
+      digit_seen[digit]++;
+      n /= 10;
+    }
+    printf("Digit:\t\t");
+    for (int i = 0; i < 10; i++)
+      printf("%d  ", i);
+    printf("\nOccurrences:\t"); // print digit counts
+    for (int i = 0; i < 10; i++)
+      printf("%d  ", digit_seen[i]);
+    printf("\n");
+  }
+
+  return 0;
+}
+
