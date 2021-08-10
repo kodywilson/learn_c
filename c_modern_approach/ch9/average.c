819a532e3c24fd75f031b26fc2a9dc397609b095
Kody Wilson <kodywilson@gmail.com>
2021-08-10 13:09:20 -0700

    ch9 average program

---
+++ c_modern_approach/ch9/average.c
@@ -0,0 +1,22 @@
+// find averages of two numbers
+#include <stdio.h>
+
+double average(double a, double b)
+{
+  return (a + b) / 2;
+}
+
+int main(void)
+{
+  double x, y, z;
+
+  printf("Please enter three numbers: ");
+  scanf("%lf %lf %lf", &x, &y, &z);
+
+  printf("Average of %g and %g is %g\n", x, y, average(x, y));
+  printf("Average of %g and %g is %g\n", x, z, average(x, z));
+  printf("Average of %g and %g is %g\n", y, z, average(y, z));
+
+  return 0;
+}
+
