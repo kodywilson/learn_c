509483326c056bbe07da86fb1eb491bfc836847c
Kody Wilson <kodywilson@gmail.com>
2021-08-13 08:39:09 -0700

    ch9 exercise 3

--- c_modern_approach/ch9/exercises/ch9_exercises.c
+++ c_modern_approach/ch9/exercises/ch9_exercises.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int check(int x, int y, int n); // ex2
+int gcd(int m, int n); // ex3
 
 int main(void)
 {
-  int x = 3, y = 5, n = 4;
+  int x = 15, y = 25, n = 100;
 
   if (check(x, y, n) == 1 )
     printf("Both %d and %d are between 0 and %d\n", x, y, n);
   else
     printf("Both %d and %d are not between 0 and %d\n", x, y, n);
+
+  printf("The GCD of %d and %d is %d\n", x, y, gcd(x, y));
 
   return 0;
 }
@@ -20,5 +23,31 @@ int check(int x, int y, int n)
     return 1;
   else
     return 0;
+}
+
+int gcd(int m, int n)
+{
+  int a, b, c;
+  if (m >= n) // set a to largest number
+  {
+    a = m;
+    b = n;
+  }
+  else
+  {
+    a = n;
+    b = m;
+  }
+  // Find the greatest common denominator
+  while (a != 0 || b != 0 || c != 0)
+  {
+    c = a % b;
+    a = b;
+    if (c == 0)
+      break;
+    b = c;
+  }
+
+  return a;
 }
 
