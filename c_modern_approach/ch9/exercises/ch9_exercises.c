67308b01ca7f6f855b04e99789c920dbebb6d5c7
Kody Wilson <kodywilson@gmail.com>
2021-08-13 09:55:40 -0700

    ch9 ex2 tighten up the function

--- c_modern_approach/ch9/exercises/ch9_exercises.c
+++ c_modern_approach/ch9/exercises/ch9_exercises.c
@@ -28,10 +28,7 @@ int main(void)
 
 int check(int x, int y, int n)
 {
-  if ( x >= 0 && y >= 0 && x <= n - 1 && y <= n - 1)
-    return 1;
-  else
-    return 0;
+  return ( x >= 0 && y >= 0 && x <= n - 1 && y <= n - 1);
 }
 
 int gcd(int m, int n)
