8f7034e3dd310d5992714bb2bc9b36313cad8ade
Kody Wilson <kodywilson@gmail.com>
2021-08-15 08:08:48 -0700

    ch9 project 1 selection sort

--- c_modern_approach/ch9/projects/sort.c
+++ c_modern_approach/ch9/projects/sort.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void selection_sort(int n, a[n]);
+
 int main(void)
 {
   int n;
@@ -13,6 +15,28 @@ int main(void)
   for (int i = 0; i < n; i++)
     scanf("%d", &a[i]);
 
+  selection_sort();
+
+  printf("Here are the %d numbers sorted least to greatest: ", n);
+  for (int i = 0; i < n; i++)
+    printf("%d ", a[i]);
+
   return 0;
+}
+
+int selection_sort(int n, a[n])
+{
+  int big = 0, position = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (a[i] > big)
+    {
+      big = a[i];
+      position = i;
+    }
+  }
+  a[position] = a[n - 1];
+  a[n - 1] = big;
+  return n - 1 < 2 ? 0 : selection_sort(n - 1, a[n]);
 }
 
