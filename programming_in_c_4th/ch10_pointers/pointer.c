ecdfe77b11591684d9d1e3d45dbcc5ab595d5a01
Kody Wilson <kodywilson@gmail.com>
2021-08-21 15:08:34 -0700

    char pointers

--- programming_in_c_4th/ch10_pointers/pointer.c
+++ programming_in_c_4th/ch10_pointers/pointer.c
@@ -2,11 +2,19 @@
 
 int main(void)
 {
+  char c = 'Q', *char_pointer = &c;
   int count = 10, *int_pointer = &count, x = *int_pointer;
 
   x = *int_pointer;
 
   printf("count = %i, x = %i\n", count, x);
+  printf("%c %c\n", c, *char_pointer);
+
+  c = '/';
+  printf("%c %c\n", c, *char_pointer);
+
+  *char_pointer = '(';
+  printf("%c %c\n", c, *char_pointer);
 
   return 0;
 }
