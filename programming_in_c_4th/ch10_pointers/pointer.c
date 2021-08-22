23ccdfd4ba9ca83b9b2499a982c143ade923da62
Kody Wilson <kodywilson@gmail.com>
2021-08-22 07:35:52 -0700

    more pointers

--- programming_in_c_4th/ch10_pointers/pointer.c
+++ programming_in_c_4th/ch10_pointers/pointer.c
@@ -16,6 +16,16 @@ int main(void)
   *char_pointer = '(';
   printf("%c %c\n", c, *char_pointer);
 
+  int i1, i2;
+  int *p1, *p2;
+
+  i1 = 5;
+  p1 = &i1;
+  i2 = *p1 / 2 + 10;
+  p2 = p1;
+
+  printf("i1 = %i, i2 = %i, *p1 = %i, *p2 = %i\n", i1, i2, *p1, *p2);
+
   return 0;
 }
 
