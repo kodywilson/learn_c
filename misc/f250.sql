ede912696d429321932893ca297997d61ce5b406
Kody Wilson <kodywilson@gmail.com>
2021-08-23 11:32:13 -0700

    better organize misc code

--- misc/f250.sql
+++
@@ -1,7 +0,0 @@
-
-{
-    "code": "Unauthorized",
-    "message": "Unauthorized",
-    "type": "tag:oracle.com,2020:error/Unauthorized",
-    "instance": "tag:oracle.com,2020:ecid/36d12e3eef8f009e30420b4a45bdd530"
-}
