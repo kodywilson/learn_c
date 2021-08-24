76f5c1ee98e20074eaec7b2c9c600c08a9927a4c
Kody Wilson <kodywilson@gmail.com>
2021-08-24 13:39:14 -0700

    format specifiers and puts with strings

--- c_modern_approach/ch13_strings/lines.c
+++ c_modern_approach/ch13_strings/lines.c
@@ -4,6 +4,11 @@ int main(void)
 {
   printf("When you come to a fork in the road, take it. "
          "--Yogi Berra\n");
+  
+  printf("%8s\n", "Yak");
+  printf("%-8s%s\n", "Yak", "Yak");
+
+  puts("This is only a test");
 
   return 0;
 }
