068e19cc2b336808d71dad1666b8080e1cd976b0
Kody Wilson <kodywilson@gmail.com>
2021-08-26 11:40:04 -0700

    define constants

--- k_and_r/ch1/reverse_celcius.c
+++ k_and_r/ch1/reverse_celcius.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0.0f
+#define UPPER 300.0f
+#define STEP 20.0f
+
 int main(void)
 {
   float fahr;
-  float lower, upper, step;
-
-  lower = 0.0f;    // lower limit of temperature scale
-  upper = 300.0f;  // upper limit
-  step = 20.0f;    // step size
 
   printf("Fahrenheit Celsius\n");
-  for (fahr = upper; fahr >= lower; fahr -= step)
+  for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
     printf("%3.0f\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
 
   return 0;
