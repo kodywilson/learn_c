5910ca6ae40c9a9ac01839eb55060a9046aa73bd
Kody Wilson <kodywilson@gmail.com>
2021-08-25 19:07:02 -0700

    celsius program use floats

--- k_and_r/celcius.c
+++ k_and_r/celcius.c
@@ -2,18 +2,18 @@
 
 int main(void)
 {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
-  lower = 0;    // lower limit of temperature scale
-  upper = 300;  // upper limit
-  step = 20;    // step size
+  lower = 0.0f;    // lower limit of temperature scale
+  upper = 300.0f;  // upper limit
+  step = 20.0f;    // step size
 
   fahr = lower;
   while (fahr <= upper)
   {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr += step;
   }
 
