5d374e1aab98403d98a4cfc7109f7c81b41f8206
Kody Wilson <kodywilson@gmail.com>
2021-08-27 14:55:25 -0700

    chapter 1 exe1.8

--- k_and_r/ch1/files.c
+++ k_and_r/ch1/files.c
@@ -6,7 +6,8 @@ int main(int argc, char *argv[])
 {
   FILE *fp; // pointer to file
   char c;   // char to read content
-  long nc, nl = 0;  // how many chars in file
+  long nb, nc, nl, nt;  // various counts
+  nb = nc = nl = nt = 0;  // start all counts at 0
   
   if (argc <= 2) // show help text
     puts("Options are cc (char count), lc (line count), wc (word count), or cat (display file) followed by file name");
@@ -24,6 +25,15 @@ int main(int argc, char *argv[])
         if (c == '\n')
           ++nl;
       printf("%s has %ld lines\n", argv[2], nl);
+    } else if (strcmp(argv[1], "count") == 0) {
+      while ((c = fgetc(fp)) != EOF)
+        switch (c)
+        {
+          case ' ': ++nb; break;
+          case '\n': ++nl; break;
+          case '\t': ++nt; break;
+        }
+      printf("%s has %ld lines %ld blanks and %ld tabs\n", argv[2], nl, nb, nt);
     }
 
     fclose(fp);
