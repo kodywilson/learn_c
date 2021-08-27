89ce93ad460f304825cd47bfa9c0f68033950783
Kody Wilson <kodywilson@gmail.com>
2021-08-27 13:32:05 -0700

    very simple character counter

--- k_and_r/ch1/files.c
+++ k_and_r/ch1/files.c
@@ -6,18 +6,21 @@ int main(int argc, char *argv[])
 {
   FILE *fp; // pointer to file
   char c;   // char to read content
+  long nc;  // how many chars in file
   
   if (argc <= 2) // show help text
-    puts("Options are lc (line count), wc (word count), or cat (display file) followed by file name");
+    puts("Options are cc (char count), lc (line count), wc (word count), or cat (display file) followed by file name");
   else if (argc == 3)
   {
     fp = fopen(argv[2], "r");
-    while ((c = fgetc(fp)) != EOF)
-      printf("%c", c);
     if (strcmp(argv[1], "cat") == 0) {
       while ((c = fgetc(fp)) != EOF)
         printf("%c", c);
+    } else if (strcmp(argv[1], "cc") == 0) {
+      for (nc = 0; fgetc(fp) != EOF; ++nc);
+      printf("%s has %ld characters\n", argv[2], nc);
     }
+
     fclose(fp);
   }
   else {
