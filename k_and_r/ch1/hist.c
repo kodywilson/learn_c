fbf269e6f80cd41517872b57404fbc9c524f2a5b
Kody Wilson <kodywilson@gmail.com>
2021-08-31 11:01:32 -0700

    ch1 exe1.14 letter count histogram

--- k_and_r/ch1/hist.c
+++ k_and_r/ch1/hist.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#define ALP 26 // number of characters in alphabet
 #define IN  1 // inside a word
 #define OUT 0 // outside a word
 #define MAX 15 // maximum word size
@@ -9,9 +10,12 @@ int main(int argc, char *argv[])
   FILE *fp;
   int c, letters, state;
   int letter_count[MAX];
+  int alphabet[ALP];
 
   for (int i = 0; i < MAX; i++) // initialize letter count array
     letter_count[i] = 0;
+  for (int i = 0; i < ALP; i++) // initialize alphabet count array
+    alphabet[i] = 0;
 
   state = OUT;
   letters = 0;
@@ -26,15 +30,29 @@ int main(int argc, char *argv[])
     }
     else
       state = IN;
-    if (state == IN)
+    if (state == IN) {
       letters++;
+      if ( c >= 65 && c <= 90 )
+        alphabet[c - 'A']++;
+      else if ( c >= 97 && c <= 122 )
+        alphabet[c - 'a']++;
+    }
   }
   fclose(fp);
 
-  printf("# Letters\tFrequency\n");
+  printf("# Letters\tFrequency\n"); // print histogram of word size frequency
   for (int i = 0; i < MAX; i++) {
     printf("%d\t\t", i + 1);
     for (int j = 0; j < letter_count[i]; j++)
+      printf("*");
+    printf("\n");
+  }
+  printf("\n");
+
+  printf("Letter\tFrequency\n"); // print histogram of letter frequency
+  for (int i = 0; i < ALP; i++) {
+    printf("%c:\t", (char) (i + 'A'));
+    for (int j = 0; j < alphabet[i]; j++)
       printf("*");
     printf("\n");
   }
