f9065acc8afa77e9693527783cb43536c1d48c77
Kody Wilson <kodywilson@gmail.com>
2021-09-02 15:24:12 -0700

    ch1 finish up maxline program

--- k_and_r/ch1/maxline.c
+++ k_and_r/ch1/maxline.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #define MAXLINE 1000    // maximum input line length
 
-//int getline(char line[], int maxline);
-//void copy(char to[], char from[]);
+int countline(char line[], int maxline);
+void copy(char to[], char from[]);
 
 // print the longest input line
 int main(int argc, char *argv[])
@@ -23,10 +23,43 @@ int main(int argc, char *argv[])
 
   while (fgets(line, sizeof(line), fp)) {
     printf("%s", line);
+    len = countline(line, MAXLINE);
+    printf("max is %d and len is %d\n", max, len);
+      if (len > max) {
+        max = len;
+        copy(longest, line);
+      }
   }
+
+  if (max > 0)
+    printf("\nLongest line: %s\n", longest);
 
   fclose(fp);
 
   return 0;
+}
+
+// countline: read a line into s, return length
+int countline(char s[], int lim)
+{
+  int c, i;
+
+  for (i = 0; (c = s[i]) != '\n' && c != '\0'; i++)
+    ;
+  /*if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';*/
+  return i;
+}
+
+// copy biggest line into longest char array
+void copy(char to[], char from[])
+{
+  int i = 0;
+
+  while ((to[i] = from[i]) != '\0')
+      ++i;
 }
 
