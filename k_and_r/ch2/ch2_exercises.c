d81e9c5a66b059f903c905b269d8e426851cf335
Kody Wilson <kodywilson@gmail.com>
2021-09-09 13:39:35 -0700

    ch2 exe2.5 any function

--- k_and_r/ch2/ch2_exercises.c
+++ k_and_r/ch2/ch2_exercises.c
@@ -9,11 +9,14 @@ int main(void)
 {
   int i;
   char s1[] = "This is turkey time", s2[] = "te";
+  char s3[] = "Another test";
 
   for (i = 0; s2[i] != '\0'; i++)
     squeeze(s1, s2[i]);
 
   printf("%s\n", s1);
+
+  printf("Position of first matching character: %d\n", any(s3, s2));
 
   return 0;
 }
@@ -30,6 +33,10 @@ void squeeze(char s[], int c)
 
 int any(char s1[], char s2[])
 {
-
+  for (int i = 0; s1[i] != '\0'; i++)
+    for (int j = 0; s2[j] != '\0'; j++)
+      if (toupper(s1[i]) == toupper(s2[j]))
+        return i + 1;
+  return -1;
 }
 
