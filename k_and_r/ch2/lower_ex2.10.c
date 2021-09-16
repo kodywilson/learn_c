b0f4a5f31b109e782948f98e093dc370f1123941
Kody Wilson <kodywilson@gmail.com>
2021-09-15 20:29:08 -0700

    k and r exe 2.10 another example

--- k_and_r/ch2/lower_ex2.10.c
+++ k_and_r/ch2/lower_ex2.10.c
@@ -5,8 +5,11 @@ void lower(char s[]);
 int main(void)
 {
   char s[] = "Hey there YOU know this is CRAZY";
+  char s2[] = "THIS IS ONLY A TEST";
   lower(s);
   printf("%s\n", s);
+  lower(s2);
+  printf("%s\n", s2);
 
   return 0;
 }
