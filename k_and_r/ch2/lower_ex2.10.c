b8cc0bd7c6ed098a8b21977ead4167712215913b
Kody Wilson <kodywilson@gmail.com>
2021-09-16 13:39:11 -0700

    creating an upper function for strings

--- k_and_r/ch2/lower_ex2.10.c
+++ k_and_r/ch2/lower_ex2.10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void lower(char s[]);
+void upper(char s[]);
 
 int main(void)
 {
@@ -10,6 +11,9 @@ int main(void)
   printf("%s\n", s);
   lower(s2);
   printf("%s\n", s2);
+
+  upper(s);
+  printf("%s\n", s);
 
   return 0;
 }
@@ -21,6 +25,17 @@ void lower(char s[])
 
   while ((c = s[i]) != '\0') {
     s[i] = (c > 64 && c < 91) ? c + 32 : c;
+    i++;
+  }
+}
+
+void upper(char s[])
+{
+  char c;
+  int i = 0;
+
+  while ((c = s[i]) != '\0') {
+    s[i] = (c > 96 && c < 123) ? c - 32 : c;
     i++;
   }
 }
