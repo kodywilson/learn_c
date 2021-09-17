56dedbb404f1ddd01a0c3742112488adf6f8202e
Kody Wilson <kodywilson@gmail.com>
2021-09-17 12:53:01 -0700

    knr ch3 exe3.1

---
+++ k_and_r/ch3/binsearch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define A_SIZE 10
+
+// prototypes
+int binsearch(int x, int v[], int n);
+
+int main(void)
+{
+  int v[A_SIZE], x = 3, y;
+
+  for (int i = 1; i <= A_SIZE; i++) // initialize array
+    v[i - 1] = i;
+
+  if ( (y = binsearch(x, v, A_SIZE)) >= 0 )
+    printf("%d found at position %d.\n", x, y);
+  else
+    printf("%d not found.\n", x);
+
+  return 0;
+}
+
+int binsearch(int x, int v[], int n)
+{
+  int low, high, mid;
+
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low + high)/2;
+    if (x < v[mid])
+      high = mid + 1;
+    else if (x > v[mid])
+      low = mid + 1;
+    else // found match
+      return mid;
+  }
+  return -1; // no match
+}
+
