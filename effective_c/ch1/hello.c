33e1003f4cee62d845644bc4e31dd0ddd116d33a
Kody Wilson <kodywilson@gmail.com>
2021-09-23 10:09:37 -0700

    hello with printf this time

--- effective_c/ch1/hello.c
+++ effective_c/ch1/hello.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main(void) {
-  if (puts("Hello, world!") == EOF)
-    return EXIT_FAILURE;
+  if (printf("%s\n", "Hello, world!") < 1 );
+  // return EXIT_FAILURE;
 
   return EXIT_SUCCESS;
 }
