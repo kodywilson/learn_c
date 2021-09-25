13e8b873269a049181c0578eebd4513cc8ad9b71
Kody Wilson <kodywilson@gmail.com>
2021-09-24 19:52:28 -0700

    effective c listing 2.1

---
+++ effective_c/ch1/swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// prototypes
+void swap(int, int);
+
+int main(void) {
+  int a = 21;
+  int b = 17;
+
+  swap(a, b);
+  printf("main: a = %d, b = %d\n", a, b);
+
+  return 0;
+}
