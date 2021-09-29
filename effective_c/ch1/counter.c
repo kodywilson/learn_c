54fef82c792ab0aa2d82093992f35043fdd73031
Kody Wilson <kodywilson@gmail.com>
2021-09-29 11:21:04 -0700

    simple counter program

--- effective_c/ch1/counter.c
+++ effective_c/ch1/counter.c
@@ -3,9 +3,15 @@
 void increment(void);
 
 int main(void) {
+  for (int i = 0; i < 5; i++)
+    increment();
+  printf("\n");
   return 0;
 }
 
 void increment(void) {
+  static unsigned int counter = 0;
+  counter++;
+  printf("%d ", counter);
 }
 
