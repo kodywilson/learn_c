28c4593a07e33d3355f5c5b7d1b7e2415dd2278b
Kody Wilson <kodywilson@gmail.com>
2021-10-26 16:57:55 -0700

    test function

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -6,7 +6,7 @@
 int main(void) {
   int i = FOO;
   int j = BAR_sq(10);
-  int k = BAR_sq(2 + 2);
+  int k = BAR_sq(3 + 3);
 
   printf("i = %d  and  j = %d  and  k = %d\n", i, j, k);
   printf("The value of func is %d\n", func());
