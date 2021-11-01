818f83be34e95b946feafcfd38609416976874b6
Kody Wilson <kodywilson@gmail.com>
2021-11-01 09:54:54 -0700

    arrays

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -7,10 +7,14 @@ int main(void) {
   int i = FOO;
   int j = BAR_sq(100);
   int k = BAR_sq(3 + 3);
+  int a[];
 
   printf("i = %d  and  j = %d  and  k = %d\n", i, j, k);
   printf("The value of func is %d\n", func());
   printf("k * 3 = %d\n", BAR_triple(k));
+
+  for (int m = 1; m < 11; m++)
+    a[m - 1] = BAR_sq(m);
 
   return 1;
 }
