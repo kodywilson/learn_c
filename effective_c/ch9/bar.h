4feb0ee058bf21f52e54062995fb67e139e39cb9
Kody Wilson <kodywilson@gmail.com>
2021-11-02 20:44:36 -0700

    more testing

--- effective_c/ch9/bar.h
+++ effective_c/ch9/bar.h
@@ -4,6 +4,7 @@
 int func(void) {
   return 1;
 }
+char letter(void) { return 'A'; }
 #endif // BAR_H
 #define BAR_sq(y) ((y) * (y))
 #define BAR_triple(x) ((x) * 3)
--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -7,7 +7,7 @@ int main(void) {
   int i = FOO;
   int j = BAR_sq(100);
   int k = BAR_sq(3 + 3);
-  int a[];
+  int a[26];
 
   printf("i = %d  and  j = %d  and  k = %d\n", i, j, k);
   printf("The value of func is %d\n", func());
