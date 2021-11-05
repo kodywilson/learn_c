ba523ed1bcd957c27f7c65615a7647ff63306892
Kody Wilson <kodywilson@gmail.com>
2021-11-04 18:52:29 -0700

    variables

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "bar.h"
 #include "bar.h" // normally duplicate inclusion is not so obvious
-#define FOO (6 + 6)
+#define FOO (30 + 30)
 
 int main(void) {
   int i = FOO;
   int j = BAR_sq(100);
   int k = BAR_sq(3 + 3);
   int a[26];
+
+  char c;
 
   printf("i = %d  and  j = %d  and  k = %d\n", i, j, k);
   printf("The value of func is %d\n", func());
@@ -17,7 +19,9 @@ int main(void) {
     a[m - 1] = BAR_sq(m);
 
   printf("The first letter of the alphabet is %c\n", letter());
-  printf("We saw above that i is %d. That would be %c on the alphabet\n", i, 'A' + a[i - 1]);
+
+  c = i + '0';
+  printf("We saw above that i is %d. That would be %c on the alphabet\n", i, c);
 
 
   return 1;
