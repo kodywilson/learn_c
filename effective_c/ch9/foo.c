a47f103627005d9d870ee2e78f1cff0853263843
Kody Wilson <kodywilson@gmail.com>
2021-11-06 11:38:27 -0700

    iterating over array

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -2,12 +2,13 @@
 #include "bar.h"
 #include "bar.h" // normally duplicate inclusion is not so obvious
 #define FOO (35 + 35)
+#define ALF 26
 
 int main(void) {
   int i = FOO;
   int j = BAR_sq(100);
   int k = BAR_sq(3 + 3);
-  int a[26];
+  int a[ALF];
 
   char c;
 
@@ -15,14 +16,17 @@ int main(void) {
   printf("The value of func is %d\n", func());
   printf("k * 3 = %d\n", BAR_triple(k));
 
-  for (int m = 1; m < 11; m++)
+  for (int m = 1; m < ALF + 1; m++)
     a[m - 1] = BAR_sq(m);
 
   printf("The first letter of the alphabet is %c\n", letter());
 
   c = i + '0';
   printf("We saw above that i is %d. That would be %c on the alphabet\n", i, c);
-
+  
+  for (int n = 0; n < ALF; n++)
+    printf("%d ", a[n]);
+  printf("\n");
 
   return 1;
 }
