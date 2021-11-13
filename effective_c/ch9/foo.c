d23e4bed251666830f4663f42533ecb872b2f86c
Kody Wilson <kodywilson@gmail.com>
2021-11-12 19:48:53 -0800

    chars and ints

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "bar.h"
 #include "bar.h" // normally duplicate inclusion is not so obvious
-#define FOO (30 + 30)
+#define FOO (27 + 27)
 #define ALF 26
 
 int main(void) {
