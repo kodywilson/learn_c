33bf3e13b35d683434f74cf5fe3c545a34648fcf
Kody Wilson <kodywilson@gmail.com>
2021-11-24 13:32:06 -0800

    minor change

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "bar.h"
 #include "bar.h" // normally duplicate inclusion is not so obvious
-#define FOO (19 + 38)
+#define FOO (19 + 39)
 #define ALF 26
 
 int main(void) {
