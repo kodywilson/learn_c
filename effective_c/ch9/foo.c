008b768d904b663f7317225d5241794a94c058ac
Kody Wilson <kodywilson@gmail.com>
2021-12-04 14:24:09 -0800

    random numbers making random characters

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -56,6 +56,8 @@ int main(void) {
   printf("Ok, now I am going to summon a random letter. It is %c this time!\n", a[r]);
   r = rand() % ALF;
   printf("Ok, now I am going to summon a random letter. It is %c this time!\n", a[r]);
+  r = rand() % ALF;
+  printf("Ok, now I am going to summon a random letter. It is %c this time!\n", a[r]);
 
   return 1;
 }
