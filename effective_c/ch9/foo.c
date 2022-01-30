39e8bc9821524d843330c3bd5092119c745353de
Kody Wilson <kodywilson@gmail.com>
2022-01-30 12:00:17 -0800

    more Amazing Grace

--- effective_c/ch9/foo.c
+++ effective_c/ch9/foo.c
@@ -122,6 +122,7 @@ int main(void) {
   puts("My chains are gone");
   puts("I've been set free");
   puts("My God, my Savior has ransomed me");
+  printf("\n");
   puts("And like a flood, His mercy rains");
   puts("Unending love, Amazing grace (grace)");
   puts("I once was lost, but now I'm found");
