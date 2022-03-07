b558391cb95bd8837e53d3442baef18ae10ba27b
Kody Wilson <kodywilson@gmail.com>
2022-03-07 15:53:57 -0800

    adding math.c

---
+++ free_dos_c/intro/math.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int top;
+  int bottom;
+  int result;
+
+  top = 25;
+  bottom = 5;
+
+  result = top / bottom;
+
+  printf("%d / %d = %d\n", top, bottom, result);
+
+  return 0;
+}
