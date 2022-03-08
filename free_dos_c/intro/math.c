4399b60f757046ca8c299e899d3f7d4aa8ded81d
Kody Wilson <kodywilson@gmail.com>
2022-03-07 16:07:00 -0800

    more math

--- free_dos_c/intro/math.c
+++ free_dos_c/intro/math.c
@@ -4,13 +4,15 @@ int main() {
   int top;
   int bottom;
   int result;
+  int remain;
 
   top = 25;
   bottom = 5;
 
   result = top / bottom;
+  remain = top % bottom;//top - result * bottom;
 
-  printf("%d / %d = %d\n", top, bottom, result);
+  printf("%d / %d = %d rem %d\n", top, bottom, result, remain);
 
   return 0;
 }
