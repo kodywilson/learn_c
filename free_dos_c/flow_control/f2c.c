14c1da3c30db6f697fe3291bb4da7f3c90847dfb
Kody Wilson <kodywilson@gmail.com>
2022-03-10 15:44:26 -0800

    working on flow control practice programs

--- free_dos_c/flow_control/f2c.c
+++ free_dos_c/flow_control/f2c.c
@@ -3,12 +3,13 @@
 int main() {
   int cels, fahr;
 
-  puts("Please enter a temperature in Fahrenheit:");
-  scanf("%d", &fahr);
+  //puts("Please enter a temperature in Fahrenheit:");
+  //scanf("%d", &fahr);
 
-  cels = (fahr - 32) * 5 / 9;
-
-  printf("The Fahrenheit temperature %d is the same as %d in Celsius.\n", fahr, cels);
+  for (fahr = 98; fahr <= 104; fahr++) {
+    cels = (fahr - 32) * 5 / 9;
+    printf("The Fahrenheit temperature %d is the same as %d in Celsius.\n", fahr, cels);
+  }
 
   return 0;
 }
