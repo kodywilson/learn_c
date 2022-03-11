bce6ae7adcdd21838102c6a5591f3f1646b07f31
Kody Wilson <kodywilson@gmail.com>
2022-03-11 12:11:12 -0800

    update even.c to use function

--- free_dos_c/functions/even.c
+++ free_dos_c/functions/even.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 // tests if number is even
-int is_even(int i) {
-  return (i % 2 == 0);
-}
+int is_even(int i);
 
 int main() {
   int num;
@@ -14,9 +12,15 @@ int main() {
   do {
     scanf("%d", &num);
 
-    if (is_even(num)) printf("%d is even\n", num);
+    if (num == 0) printf("Goodbye!\n");
+    else if (is_even(num)) printf("%d is even\n", num);
     else printf("%d is odd\n", num);
   } while (num != 0);
 
   return 0;
+}
+
+// tests if number is even
+int is_even(int i) {
+  return (i % 2 == 0);
 }
