58671794175863230c87ab5e0be0a7b2188e296c
Kody Wilson <kodywilson@gmail.com>
2022-03-11 16:58:20 -0800

    looping hello function

--- free_dos_c/functions/hello.c
+++ free_dos_c/functions/hello.c
@@ -5,7 +5,23 @@ int hello() {
   return 0;
 }
 
+int loophello() {
+  int iter;
+
+  for (iter = 1; iter <= 3; iter++) printf("%d - Hello!\n", iter);
+
+  return 0;
+}
+
 int main() {
+  int iter;
+
   hello();
+
+  for (iter = 1; iter <= 3; iter++) {
+    printf("Loop %d\n", iter);
+    loophello();
+  }
+
   return 0;
 }
