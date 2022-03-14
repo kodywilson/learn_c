1359f01fed9b849fb705741dd5bd40baaae999ad
Kody Wilson <kodywilson@gmail.com>
2022-03-14 11:23:17 -0700

    functions - practice program 2

--- free_dos_c/functions/fact.c
+++ free_dos_c/functions/fact.c
@@ -2,13 +2,13 @@
 
 int fact(int i);
 
-//int factr(int i);
+int factr(int i);
 
 int main() {
   int numby = 5;
 
   printf("The factorial of %d is %d\n", numby, fact(numby));
-  //printf("Using recursive function, the factorial of %d is %d\n", numby, factr(numby));
+  printf("Using recursive function, the factorial of %d is %d\n", numby, factr(numby));
 
   return 0;
 }
@@ -19,4 +19,11 @@ int fact(int i) {
   for (i = fact; i > 1; i--) fact = fact * (i - 1);
 
   return fact;
+}
+
+int factr(int i) {
+  switch (i) {
+    case 0: return 1;
+    default: return i * factr(i - 1);
+  }
 }
