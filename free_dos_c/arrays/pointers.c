be206dcfaea416714d77fb110cd95e45235e3097
Kody Wilson <kodywilson@gmail.com>
2022-03-15 13:44:11 -0700

    intro to pointers and arrays

---
+++ free_dos_c/arrays/pointers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int *numlist;
+  int numlist_len = 10;
+  int i;
+
+  // allocate memory
+  numlist = (int *) malloc(numlist_len * sizeof(int));
+
+  if (numlist == NULL) {
+    puts("something went wrong during malloc");
+    return 1;
+  }
+
+  for ( i = 0; i < numlist_len; i++) {
+    numlist[i] = i + 100;
+    printf("numlist[%d] is now %d\n", i, numlist[i]);
+  }
+
+  // release memory
+  free(numlist);
+
+  return 0;
+}
