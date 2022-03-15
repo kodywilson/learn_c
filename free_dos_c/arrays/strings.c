af778fca24c3e2acb5d06cdb3fb6f5da5e07e805
Kody Wilson <kodywilson@gmail.com>
2022-03-15 10:49:31 -0700

    custom string length function

--- free_dos_c/arrays/strings.c
+++ free_dos_c/arrays/strings.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+int string_length(char string[]) {
+  int len = 0;
+
+  // find null character
+  while (string[len] != '\0') len++;
+
+  return len;
+}
+
 int main() {
-  char hello[] = "Hi there!";
+  char hello[] = "Hello world!";
 
   puts(hello);
 
@@ -9,6 +18,8 @@ int main() {
 
   puts("<123456789> -- counting characters");
   puts("<012345678> -- positions in the array");
+
+  printf("The string is %d characters long\n", string_length(hello));
 
   return 0;
 }
