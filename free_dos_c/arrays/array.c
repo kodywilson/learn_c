9abb13a2b38029136cfb6bd7018b60a011550477
Kody Wilson <kodywilson@gmail.com>
2022-03-15 10:36:50 -0700

    more arrays

--- free_dos_c/arrays/array.c
+++ free_dos_c/arrays/array.c
@@ -6,7 +6,10 @@ int main() {
   int tenlist[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
   // print contents of tenlist
-  for (int i = 0; i < 10; i++) printf("tenlist[%d] = %d\n", i, tenlist[i]);
+  for (int i = 0; i < 10; i++) {
+    numlist[i] = tenlist[i];
+    printf("numlist[%d] = %d\n", i, numlist[i]);
+  }
 
   return 0;
 }
