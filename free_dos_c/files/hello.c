35367a1c6fc96c96a3b9efa0d97c2c8763ef5345
Kody Wilson <kodywilson@gmail.com>
2022-03-24 17:34:07 -0700

    using getopts

--- free_dos_c/files/hello.c
+++ free_dos_c/files/hello.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 int main(int argc, char **argv) {
+  int i;
 
-  for (int i = 0; i < argc; i++) {
+  while ((i = getopt(argc, argv, "hHqQ")) != EOF) {
+    switch(i) {
+      case 'h':
+      case 'H': puts("You chose help"); break;
+      case 'q':
+      case 'Q': puts("You chose to quit"); break;
+      default: puts("Unable to detemine what you want.");
+    }
+  }
+
+  for (int i = optind; i < argc; i++) {
     printf("%d: %s\n", i, argv[i]);
   }
 
