583304060c89a4d15a986f5c5b2195e023b036c7
Kody Wilson <kodywilson@gmail.com>
2022-03-28 13:14:11 -0700

    simple cp style program

--- free_dos_c/files/cp.c
+++ free_dos_c/files/cp.c
@@ -9,21 +9,40 @@ void copyfile(FILE *from, FILE *to) {
 }
 
 int main(int argc, char **argv) {
-  int i;
-  FILE *fileptr;
+  FILE *src;
+  FILE *dst;
 
-  // loop through argv
-
-  for (i = 1; i < argc; i++) {
-    fileptr = fopen(argv[i], "r");
-
-    if (fileptr == NULL) fprintf(stderr, "Cannot open file %s\n", argv[i]);
-    else {
-      copyfile(fileptr, stdout);
-      fclose(fileptr);
-      printf("\n");
-    }
+  // check for valid arguments
+  
+  if (argc != 3) {
+    fprintf(stderr, "Wrong number of command line arguments\n");
+    fprintf(stderr, "usage: cp source destination\n");
+    return 1;
   }
+
+  // open the files
+
+  src = fopen(argv[1], "r");
+
+  if (src == NULL) {
+    fprintf(stderr, "Cannot open file %s for reading\n", argv[1]);
+    return 1;
+  }
+
+  dst = fopen(argv[2], "w");
+
+  if (dst == NULL) {
+    fprintf(stderr, "Cannot open file %s for writing\n", argv[2]);
+    fclose(src);
+    return 2;
+  }
+  
+  // copy the file
+  copyfile(src, dst);
+
+  // close files
+  fclose(dst);
+  fclose(src);
 
   return 0;
 }
