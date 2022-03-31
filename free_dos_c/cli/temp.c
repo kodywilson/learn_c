dbf85097e3ff79e3251c21bb22f49f517a1a2538
Kody Wilson <kodywilson@gmail.com>
2022-03-31 13:40:45 -0700

    finish up temp conversion program

--- free_dos_c/cli/temp.c
+++ free_dos_c/cli/temp.c
@@ -7,11 +7,22 @@ void print_usage() {
   exit(2);
 }
 
+void print_fahrenheit(float temp) {
+  float f = ((temp * 9) / 5) + 32;
+  printf("%.2f centigrade is %.2f fahrenheit\n", temp, f);
+}
+
+void print_centigrade(float temp) {
+  float f = ((temp - 32) * 5) / 9;
+  printf("%.2f fahrenheit is %.2f centigrade\n", temp, f);
+}
+
 int main(int argc, char **argv) {
+  if (argc < 3) print_usage();
   int option;
   int cflag = 0, fflag = 0;
 
-  while ((option = getopt(argc, argv, "cf")) != -1) {
+  while ((option = getopt(argc, argv, "c:f:")) != -1) {
     switch (option) {
       case 'c': 
         if (cflag) print_usage();
@@ -19,14 +30,14 @@ int main(int argc, char **argv) {
           cflag++;
           fflag++;
         }
-        printf("You want centigrade\n"); break;
+        print_fahrenheit(atof(optarg)); break;
       case 'f': 
         if (fflag) print_usage();
         else {
           fflag++;
           cflag++;
         }
-        printf("You want fahrenheit\n"); break;
+        print_centigrade(atof(optarg)); break;
       default: printf("Usage: %s -c or -f temp_to_convert\n", argv[0]);
     }
   }
