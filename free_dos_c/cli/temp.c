15799585883360f79195b0461ab4b740b00c071f
Kody Wilson <kodywilson@gmail.com>
2022-03-31 13:27:07 -0700

    more work on temp conversion

--- free_dos_c/cli/temp.c
+++ free_dos_c/cli/temp.c
@@ -4,11 +4,28 @@
 
 int main(int argc, char **argv) {
   int option;
+  int cflag = 0, fflag = 0;
 
   while ((option = getopt(argc, argv, "cf")) != -1) {
     switch (option) {
-      case 'c': printf("You want centigrade\n"); break;
-      case 'f': printf("You want fahrenheit\n"); break;
+      case 'c': 
+        if (cflag) {
+          printf("Only one option\n");
+          exit(1);
+        } else {
+          cflag++;
+          fflag++;
+        }
+        printf("You want centigrade\n"); break;
+      case 'f': 
+        if (fflag) {
+          printf("Only one option\n");
+          exit(1);
+        } else {
+          fflag++;
+          cflag++;
+        }
+        printf("You want fahrenheit\n"); break;
       default: printf("Usage: %s -c or -f temp_to_convert\n", argv[0]);
     }
   }
