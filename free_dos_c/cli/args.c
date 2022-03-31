5a89c39ba0321431403c64d027b3b07257b513de
Kody Wilson <kodywilson@gmail.com>
2022-03-31 16:41:22 -0700

    finish up simple args program

--- free_dos_c/cli/args.c
+++ free_dos_c/cli/args.c
@@ -6,6 +6,11 @@ int main(int argc, char **argv) {
   printf("argc = %d\n", argc);
   puts("Let's see what is in argv\n");
 
+  if (argc < 2) {
+    printf("Not enough arguments\n");
+    return 1;
+  }
+
   for (int i = 1; i < argc; i++) {
     printf("argv[%d]: %s\n", i, argv[i]);
     total += atoi(argv[i]);
