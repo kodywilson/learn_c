8bfd26c8d0634e0e68c5c8054441ff76a7fafb0f
Kody Wilson <kodywilson@gmail.com>
2022-04-01 14:14:56 -0700

    more incrementers

--- free_dos_c/advanced/incr.c
+++ free_dos_c/advanced/incr.c
@@ -19,5 +19,14 @@ int main() {
   printf("a is %d\n", a);
   printf("b is %d\n", b);
 
+  // show differences between --n and n--
+
+  printf("a-- is %d\n", a--);
+  printf("--b is %d\n", --b);
+
+  // print values again
+
+  printf("a is %d\n", a);
+  printf("b is %d\n", b);
   return 0;
 }
