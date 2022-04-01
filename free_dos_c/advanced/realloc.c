1ac32b03e2a1baa9003bf37d6959a2f5daff88c8
Kody Wilson <kodywilson@gmail.com>
2022-04-01 11:13:28 -0700

    resize array with realloc

--- free_dos_c/advanced/realloc.c
+++ free_dos_c/advanced/realloc.c
@@ -25,7 +25,30 @@ int main() {
   }
   printf("\n");
 
-  free(array);
+  // make the array bigger
+
+  newsize = size + 50;
+
+  newarray = (int *) realloc(array, newsize * sizeof(int));
+
+  if (newarray == NULL) {
+    fprintf(stderr, "Could not allocate memory with realloc\n");
+    fprintf(stderr, "...but the old pointer is still ok\n");
+  } else {
+    array = newarray;
+    size = newsize;
+  }
+
+  printf("the array is now %d size\n", size);
+
+  // store some data
+  for (int i = 0; i < size; i++) {
+    array[i] = i + 1000;
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+
+  free(array); // release memory
 
   return 0;
 }
