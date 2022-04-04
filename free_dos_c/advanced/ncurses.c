b6bd62193e8f95ee3d7a7732ec2e7f48b7e8ea47
Kody Wilson <kodywilson@gmail.com>
2022-04-04 16:38:36 -0700

    more ncurses

--- free_dos_c/advanced/ncurses.c
+++ free_dos_c/advanced/ncurses.c
@@ -3,7 +3,7 @@
 int main() {
   initscr();                  // start curses mode
   printw("Hello World!!!");   // Print Hello World
-  refresh();                  // Put on screen
+  refresh();                  // Put on screen // like fflush in conio
   getch();                    // Wait for user input
   endwin();                   // end curses mode
 
