b4d28a36c5441072dffff9c41c947eb36cd74e40
Kody Wilson <kodywilson@gmail.com>
2022-04-06 11:29:04 -0700

    finish up hello.c

--- misc/ncurses/hello.c
+++ misc/ncurses/hello.c
@@ -3,9 +3,17 @@
 int main() {
   initscr();  // initialize the screen, set up memory, clear screen
 
+  printw("This is a test\n"); // print to window
+  addstr("Hello World!\n"); // add string
+  addstr("Yo!\n");
+
   refresh(); // refreshes screen with any updates
 
-  getch(); // waits for input from user
+  int ch = getch(); // waits for input from user
+
+  printw("\nYou pressed %c\n", (char) ch);
+
+  getch();
 
   endwin(); // deallocates memory and ends ncurses
 
