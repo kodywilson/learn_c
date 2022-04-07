98d0ada7c5edef911e0acb865648648e292e1f80
Kody Wilson <kodywilson@gmail.com>
2022-04-07 12:41:36 -0700

    move text to middle of the screen

--- misc/ncurses/environment.c
+++ misc/ncurses/environment.c
@@ -12,11 +12,15 @@ int main() {
 
   int y, x, yBeg, xBeg, yMax, xMax;
 
+  WINDOW *win = newwin(10, 20, 10, 10);
+
   getyx(stdscr, y, x);
-  getbegyx(stdscr, yBeg, xBeg);
+  getbegyx(win, yBeg, xBeg);
   getmaxyx(stdscr, yMax, xMax);
 
-  printw("%d %d %d %d %d %d", y, x, yBeg, xBeg, yMax, xMax);
+  // mvprintw(yMax/2, xMax/2, "Hello");
+
+  mvprintw(yMax/2, xMax/2, "%d %d %d %d %d %d", y, x, yBeg, xBeg, yMax, xMax); // DEBUG
 
   getch();
   endwin();
