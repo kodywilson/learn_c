2a359158aa37aa941fd2e471812880648091b3fc
Kody Wilson <kodywilson@gmail.com>
2022-04-06 18:02:11 -0700

    playing with attributes and colors

--- misc/ncurses/attr_and_colors.c
+++ misc/ncurses/attr_and_colors.c
@@ -2,14 +2,32 @@
 
 int main() {
 
-  //start_ncurses(true, true);
   initscr();
+  // check if terminal supports color
+  if (!has_colors()) {
+    printw("Terminal does not support color");
+    getch();
+    return -1;
+  }
+
+  start_color(); // setup color
+
+  init_pair(1, COLOR_RED, COLOR_BLACK);
 
   // A_STANDOUT and A_REVERSE are same on my terminal
   // A_BLINK works on my terminal
-  attron(A_BLINK);
+  attron(COLOR_PAIR(1));
   printw("This is some sample text");
-  attroff(A_BLINK);
+  attroff(COLOR_PAIR(1));
+
+  if (can_change_color()) {
+    printw("Can change color - turning red to cyan");
+    init_color(COLOR_RED, 9, 999, 999); // changes red to cyan
+  }
+
+  attron(COLOR_PAIR(1));
+  printw("This is more sample text");
+  attroff(COLOR_PAIR(1));
 
   getch();
 
