d847cadaedfc2e9538418bd7eb329b71c7784a4a
Kody Wilson <kodywilson@gmail.com>
2022-04-08 17:52:56 -0700

    show menu choice

--- misc/ncurses/menu.c
+++ misc/ncurses/menu.c
@@ -43,17 +43,8 @@ int main() {
     if(choice == 10)
       break;
   }
-  // get user input
-  int c = wgetch(menuwin);
-  // KEY_UP KEY_LEFT KEY_F(function key) KEY_DOWN KEY_RIGHT
-  if (c == KEY_UP) {
-    mvwprintw(menuwin, 1, 1, "You pressed the up key!");
-    wrefresh(menuwin);
-  }
 
-  // mvprintw(yMax/2, xMax/2, "Hello");
-
-  //mvprintw(yMax/2, xMax/2, "%d %d %d %d %d %d", y, x, yBeg, xBeg, yMax, xMax); // DEBUG
+  printw("Your choice was: %s", choices[highlight]);
 
   getch();
   endwin();
