3896385c784cf6ea5de54c12de33ff4b386aaa9f
Kody Wilson <kodywilson@gmail.com>
2022-04-08 17:49:44 -0700

    finish menu with bounds checking

--- misc/ncurses/menu.c
+++ misc/ncurses/menu.c
@@ -14,10 +14,35 @@ int main() {
   // Create window for input
   WINDOW *menuwin = newwin(6, xMax-12, yMax-8, 5);
   box(menuwin, 0, 0);
-  refresh();
-  wrefresh(menuwin);
+  refresh();  // refresh screen with updates
+  wrefresh(menuwin);  // refresh window with updates
   keypad(menuwin, true); // enables arrow keys
 
+  char *choices[3] = {"Walk", "Jog", "Run"};
+  int choice;
+  int highlight = 0;
+
+  while(1) {
+    for (int i = 0; i < 3; i++) {
+      if (i == highlight) wattron(menuwin, A_REVERSE);
+      mvwprintw(menuwin, i+1, 1, choices[i]);
+      wattroff(menuwin, A_REVERSE);
+    }
+    choice = wgetch(menuwin);
+
+    switch(choice) {
+      case KEY_UP:
+        if (highlight > 0) highlight--;
+        break;
+      case KEY_DOWN:
+        if (highlight < 2) highlight++;
+        break;
+      default:
+        break;
+    }
+    if(choice == 10)
+      break;
+  }
   // get user input
   int c = wgetch(menuwin);
   // KEY_UP KEY_LEFT KEY_F(function key) KEY_DOWN KEY_RIGHT
