3b767a2e70fadfef4af7b53ff204018288838c50
Kody Wilson <kodywilson@gmail.com>
2022-04-11 17:33:42 -0700

    player in box with bounds detection

--- misc/ncurses/movement.c
+++ misc/ncurses/movement.c
@@ -3,7 +3,7 @@
 #include <ncurses.h>
 
 struct Player {
-  char rep[2];
+  char rep;
   int y, x, old_y, old_x;
 };
 
@@ -13,35 +13,47 @@ int main() {
 
   initscr();
   noecho();
+  cbreak();
   curs_set(0); // set cursor to invisible
 
   // get terminal stats
-  int choice, yMax, xMax;
-  getmaxyx(stdscr, yMax, xMax);
+  int choice, yMax, xMax, yMax_term, xMax_term;
+  getmaxyx(stdscr, yMax_term, xMax_term);
   keypad(stdscr, true);
 
-  // draw player on the screen
-  bob.y = bob.x = 0;
-  bob.rep[0] = '@';
-  bob.rep[1] = '\0';
+  // create room for player to start in
+  WINDOW *playwin = newwin(20, 50, (yMax_term/2)-10, xMax_term/10); // will change on terminal resize
+  box(playwin, 0, 0);
+  refresh();
+  wrefresh(playwin);
 
-  while (1) {
-    mvprintw(bob.y, bob.x, bob.rep);
+  // draw player on the screen
+  getmaxyx(playwin, yMax, xMax);
+  bob.y = 1;
+  bob.x = 1;
+  bob.rep = '@';
+
+  // start game - handle movement and wall detection
+  do {
+    mvwaddch(playwin, bob.y, bob.x, bob.rep);
+    wrefresh(playwin);
     bob.old_y = bob.y; // track previous position so we can clear it
     bob.old_x = bob.x;
     choice = getch();
     switch (choice) {
-      case KEY_UP: if (bob.y > 0) bob.y--; break;
-      case KEY_DOWN: if (bob.y < yMax) bob.y++; break;
-      case KEY_RIGHT: if (bob.x < xMax) bob.x++; break;
-      case KEY_LEFT: if (bob.x > 0) bob.x--; break;
+      case KEY_UP: if (bob.y > 1) bob.y--; break;
+      case KEY_DOWN: if (bob.y < yMax - 2) bob.y++; break;
+      case KEY_RIGHT: if (bob.x < xMax - 2) bob.x++; break;
+      case KEY_LEFT: if (bob.x > 1) bob.x--; break;
       default: break;
     }
     if (choice == 113) break;
-    mvaddch(bob.old_y, bob.old_x, ' ');
+    mvwaddch(playwin, bob.old_y, bob.old_x, ' ');
+    wrefresh(playwin);
   }
+  while (1); 
   
-  // getch(); // maybe add game over screen?
+  //getch(); // maybe add game over screen?
 
   endwin();
 
