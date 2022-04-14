92e8b900978718c7d705f52622c4001f393734be
Kody Wilson <kodywilson@gmail.com>
2022-04-14 13:52:49 -0700

    changing the background color

---
+++ misc/ncurses/dan_g/03-07_bgcolor.c
@@ -0,0 +1,15 @@
+#include <ncurses.h>
+
+int main() {
+  initscr();
+
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+  bkgd(COLOR_PAIR(1));
+  addstr("This is some text");
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
