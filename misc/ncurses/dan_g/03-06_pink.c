30bf86726c1e8badf7ecab2fb61c380ef1b8da83
Kody Wilson <kodywilson@gmail.com>
2022-04-14 13:49:35 -0700

    creating new colors

---
+++ misc/ncurses/dan_g/03-06_pink.c
@@ -0,0 +1,22 @@
+#include <ncurses.h>
+
+#define PINK 1          // first new color  
+#define PINK_ON_BLACK 1 // first color pair
+
+int main() {
+  initscr();
+
+  start_color();
+  if (!can_change_color())
+    addstr("This probably won't work...\n");
+
+  init_color(PINK, 1000, 750, 750);  // should be pink
+  init_pair(1, PINK, COLOR_BLACK);
+  attrset(COLOR_PAIR(PINK_ON_BLACK));
+  printw("This is the new color %d.\n", PINK);
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
