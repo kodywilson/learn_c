ee3b923ad02fe0518cdabde493209b683e12ad22
Kody Wilson <kodywilson@gmail.com>
2022-04-15 11:37:42 -0700

    beep and flash

---
+++ misc/ncurses/dan_g/03-09_notice.c
@@ -0,0 +1,18 @@
+#include <ncurses.h>
+
+int main() {
+  initscr();
+
+  addstr("Attention!\n");
+  beep();
+  refresh();
+  getch();
+
+  addstr("I said, ATTENTION!\n");
+  flash();
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
