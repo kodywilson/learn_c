86946c681aba75db59d88472f0453355b0dfb5a6
Kody Wilson <kody.wilson@oracle.com>
2022-04-27 14:03:11 -1000

    taking a look at Unicode

---
+++ misc/ncurses/dan_g/04-11_unicode.c
@@ -0,0 +1,21 @@
+#define _XOPEN_SOURCE_EXTENDED 1
+#include <ncurses.h>
+#include <locale.h>
+
+int main() {
+  cchar_t heart = { A_NORMAL, L"\u2665" };
+
+  // activate Unicode
+  setlocale(LC_CTYPE, "en_US.UTF-8");
+
+  initscr();
+
+  addstr("I ");
+  add_wch(&heart);
+  addstr(" Ncurses");
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
