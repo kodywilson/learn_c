fc8ad79f153a6af50a6e9e3842269013aadf85e8
Kody Wilson <kodywilson@gmail.com>
2022-04-29 17:13:47 -0700

    deleting lines and characters

---
+++ misc/ncurses/dan_g/06/06-09_text7.c
@@ -0,0 +1,35 @@
+#include <ncurses.h>
+
+int main() {
+
+  int x;
+
+  char text1[] = "This is the first line\n";
+  char text2[] = "Line two here\n";
+  char text3[] = "The third line\n";
+  char text4[] = "Fourth line here\n";
+  char text5[] = "And the fifth line\n";
+
+  initscr();
+
+  addstr(text1);
+  addstr(text2);
+  addstr(text3);
+  addstr(text4);
+  addstr(text5);
+  refresh();
+  getch();
+
+  // now remove a word
+  move(3, 7);
+  for (x = 0; x < 5; x++) {
+    delch();
+    refresh();
+    napms(250);
+  }
+  getch();
+
+  endwin();
+  return 0;
+
+}
