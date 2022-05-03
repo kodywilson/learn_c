5d9b9be2c9f6f799c1afda6adc09b7e0ff7cd1ff
Kody Wilson <kodywilson@gmail.com>
2022-05-03 14:40:07 -0700

    creating borders

---
+++ misc/ncurses/dan_g/09/09-08_border.c
@@ -0,0 +1,14 @@
+#include <ncurses.h>
+
+int main() {
+  initscr();
+
+  border(0, 0, 0, 0, 0, 0, 0, 0);
+  move(1, 1);
+  addstr("Now that's a swell border!");
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
