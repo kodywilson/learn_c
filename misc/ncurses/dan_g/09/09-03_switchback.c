c2ceff9226c7ae4c67a7ae1c669a54e13e52c264
Kody Wilson <kodywilson@gmail.com>
2022-05-03 10:39:48 -0700

    creating windows and switching views

---
+++ misc/ncurses/dan_g/09/09-03_switchback.c
@@ -0,0 +1,39 @@
+#include <ncurses.h>
+
+int main() {
+  int ch;
+  WINDOW *second;
+
+  initscr();
+  start_color();
+
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+  init_pair(2, COLOR_WHITE, COLOR_RED);
+
+  // create and configure second window
+  second = newwin(0, 0, 0, 0);
+  if (second == NULL) {
+    endwin();
+    puts("Unable to create window");
+    return 1;
+  }
+
+  wbkgd(second, COLOR_PAIR(2));
+  waddstr(second, "This is the second window\n");
+
+  do {
+    // setup standard screen
+    bkgd(COLOR_PAIR(1));
+    addstr("This is the standard screen\n");
+    addstr("Press ~ to exit, any other key to switch");
+    refresh();
+    ch = getch();
+
+    // show second window
+    wrefresh(second);
+    getch();
+  } while ( ch != '~' );
+
+  endwin();
+  return 0;
+}
