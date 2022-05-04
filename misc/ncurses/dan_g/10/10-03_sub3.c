ebfc045bfed44cb98ca4467ce98f61a1003c3658
Kody Wilson <kodywilson@gmail.com>
2022-05-04 09:57:04 -0700

    sub windows

--- misc/ncurses/dan_g/10/10-03_sub3.c
+++ misc/ncurses/dan_g/10/10-03_sub3.c
@@ -5,7 +5,7 @@ int main() {
 
   initscr();
 
-  sub = subwin(stdscr, LINES / 2, COLS / 2, LINES / 4, COLS / 4);
+  sub = derwin(stdscr, LINES / 2, COLS / 2, LINES / 4, COLS / 4);
   if (sub == NULL) {
     endwin();
     puts("Unable to create subwindow");
@@ -14,11 +14,19 @@ int main() {
 
   // draw a box around stdscr
   box(stdscr, 0, 0);
-  mvaddstr(1, 1, "This is the main window");
+  addstr("I'm writing text\n");
+  addstr("to the standard screen.");
+  refresh();
+  getch();
   // put text to the subwindow
   box(sub, 0, 0);
   mvwaddstr(sub, 1, 1, "I'm in the subwindow.");
-  refresh();
+  wrefresh(sub);
+  getch();
+
+  // clear subwindow
+  wclear(sub);
+  wrefresh(sub);
   getch();
 
   endwin();
