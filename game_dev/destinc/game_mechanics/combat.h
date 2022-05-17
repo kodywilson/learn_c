879094f5589036ebf618e1da704641e3615458ce
Kody Wilson <kodywilson@gmail.com>
2022-05-17 13:23:47 -0700

    added attack and player turn functions, need mob turn next

--- game_dev/destinc/game_mechanics/combat.h
+++ game_dev/destinc/game_mechanics/combat.h
@@ -1,9 +1,10 @@
 // Functions related to combat
 // selecting foe, handling battle rounds, etc.
 
+#define AC_BASE 10
 #define COMBAT_PROBABILITY 15 // dial it down for less monsters
 
-int actions(char choices[MAX_CHOICES][MAX_CHOICE_LEN], int choice_key[MAX_CHOICES], char *role) {
+int actions(char *role) {
   int num_choices = 0;
 
   if (strcmp(role, "Cleric") == 0) {
@@ -33,6 +34,51 @@ int actions(char choices[MAX_CHOICES][MAX_CHOICE_LEN], int choice_key[MAX_CHOICE
       }
 
   return num_choices;
+}
+
+int attack(pc *attacker, pc *target) {
+  int ac, damage, roll;
+
+  ac = AC_BASE + target->dodge + target->armor;                      // Base AC + bonuses
+  roll = dice(1, 20) + attacker->dmg + ((attacker->str - 10) / 2);   // d20 + bonuses
+
+  if (roll >= ac ) { // may need to drop the damage bonus and only use for to hit
+    damage = dice(1, 3) + attacker->dmg + ((attacker->str - 10) / 2);
+    target->cur_hp-=damage;
+  } else {
+    damage = 0;
+  }
+
+  return damage; // later, find a way to reflect damage absorption. For now, less than 1 means they missed
+}
+
+// handle player actions and battle choices, return choice
+int player_turn(WINDOW *select, WINDOW *game_text, pc *player, pc *monster, char *combat_prompt) {
+  int choice = 2, player_damage = 0;
+  
+  num_choices = actions(player->role);
+  choice = choose(select, num_choices, combat_prompt);
+  switch (choice_key[choice]) {
+    case 0: mvwprintw(game_text, 3, 1, "You swing at %s", monster->name);
+            wrefresh(game_text);
+            napms(250);
+            player_damage = attack(player, monster);
+            if (player_damage > 0) {
+              mvwprintw(game_text, 4, 1, "You hit %s for %d damage!", monster->name, player_damage);
+            } else {
+              mvwprintw(game_text, 4, 1, "The %s dodges your strike.", monster->name);
+            }
+            wrefresh(game_text);
+            napms(250);
+            break;
+    case 2: mvwprintw(game_text, 4, 1, "You run as fast as you can away from %s!", monster->name);
+            wrefresh(game_text);
+            napms(250);
+            break;
+    default: break;
+  }
+
+  return choice;
 }
 
 void combat(WINDOW *game_text, WINDOW *select, WINDOW *stats, pc *player, int environ) {
@@ -68,44 +114,16 @@ void combat(WINDOW *game_text, WINDOW *select, WINDOW *stats, pc *player, int en
     // roll to see who goes first - bonus to dex, int, wis, and dodge (good for rogues)
     // formula is d20 + mods > x = player goes first
     // for now, just start with player and takes turns, add check for who goes first later
-    if ((dice(1, 20) + player->dodge) > 0) { // player always wins for now
+    if ((dice(1, 20) + player->dodge + player->intel) > 10) { // help rogues and wizards not die
       // you get to go first this round!
-      // this should be a function, player attack
-      num_choices = actions(choices, choice_key, player->role);
-      choice = choose(select, num_choices, combat_prompt);
-      switch (choice_key[choice]) {
-        case 0: mvwprintw(game_text, 3, 1, "You swing at %s", monster.name);
-                wrefresh(game_text);
-                napms(250);
-                // 2 checks, first to bypass their dodge, then to hit
-                if (dice(1, 20) > monster.dodge) { // ok, they didn't dodge, now check armor
-                  if (dice(1, 20) > (10 + monster.armor - ((player->str - 10) % 2))) {
-                    // formula = 1d2 + player damage bonus + strength bonus (1 for every 2 points over 10 str)
-                    player_damage = dice(1, 3) + player->dmg + ((player->str - 10) % 2);
-                    monster.cur_hp-=player_damage;
-                    mvwprintw(game_text, 4, 1, "You hit %s for %d damage!", monster.name, player_damage);
-                  } else {
-                    mvwprintw(game_text, 4, 1, "The %s dodges your strike.", monster.name);
-                  }
-                } else {
-                  mvwprintw(game_text, 4, 1, "The %s dodges your strike.", monster.name);
-                }
-                wrefresh(game_text);
-                napms(250);
-                break;
-        case 2: mvwprintw(game_text, 4, 1, "You run as fast as you can away from %s!", monster.name);
-                wrefresh(game_text);
-                napms(250);
-                flee = 1;
-                break;
-        default: break;
-      }
-    }// else {
+      choice = player_turn(select, game_text, player, &monster, combat_prompt);
+      if (choice == 2) break; // get out, you fled
+    } //else {
       // monster attacks now
       // create function, monster attack - for now just a simple roll
       // two checks to see if monster hits the player. First dodge chance and then against armor
       if (dice(1, 20) > player->dodge) { // ok, they didn't dodge, now check armor
-        if (dice(1, 20) > 10 + player->armor) {
+        if (dice(1, 20) > AC_BASE + player->armor) {
           // formula = 1d2 + mob damage bonus + strength bonus (1 for every 2 points over 10 str)
           mob_damage = dice(1, 2) + monster.dmg + ((monster.str - 10) % 2);
           player->cur_hp-=mob_damage;
