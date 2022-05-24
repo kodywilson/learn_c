5bc48c3dda1e08d1969e6938238d096f65c37879
Kody Wilson <kodywilson@gmail.com>
2022-05-24 11:07:22 -0700

    laying the framework for animations

---
+++ game_dev/destinc/art/assets.h
@@ -0,0 +1,30 @@
+// later this will we broken out into different categories
+// for now it will be weapons, spell effects, mobs, etc.
+
+#define PIC_HEIGHT 7
+#define PIC_WIDTH  12
+
+// A tiny dungeon for testing
+char wizard[PIC_HEIGHT][PIC_WIDTH] = {
+  "            ",
+  "            ",
+  "    _^_     ",
+  "    (\")   . ",
+  "  __| |\\_/  ",
+  "   /  \\     ",
+  "  /_|_|_    "
+};
+/*
+      _^_
+      (")   .
+    __| |\_/
+     /  \
+    /_|_|_
+
+
+       _^_
+       (")
+     \/ /\___.
+     /  \
+    /_/_/_
+/*
