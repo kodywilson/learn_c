85d3e97f75dcead31463454115126bd235ab7584
Kody Wilson <kodywilson@gmail.com>
2022-06-03 16:55:06 -0700

    reading and writing structs to file

--- misc/files/employees_out.c
+++ misc/files/employees_out.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct employee
 {
@@ -11,8 +12,10 @@ struct employee
 
 int main()
 {
+    int count = 0;
     FILE *fp;
     fp = fopen("employee.txt", "rb");
+    struct employee emps[2];
 
     if(fp == NULL)
     {
@@ -24,11 +27,18 @@ int main()
 
     while( fread(&emp, sizeof(emp), 1, fp) == 1 )
     {
+        //count++;
         printf("Name: %s \n", emp.name);
+        //strcpy(emps[count].name, emp.name);
         printf("Designation: %s \n", emp.designation);
+        strcpy(emps[count].designation, emp.designation);
         printf("Age: %d \n", emp.age);
         printf("Salary: %.2f \n\n", emp.salary);
+        count++;
     }
+
+    printf("We found %d employee records.\n", count);
+    printf("Name of first employee was %s\n", emps[0].designation);
 
     fclose(fp);
     return 0;
