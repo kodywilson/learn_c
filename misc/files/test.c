e19a5eeb4094448efcae6f5f7c97a7a4a3f49151
Kody Wilson <kodywilson@gmail.com>
2022-06-19 18:55:48 -0700

    minor change

--- misc/files/test.c
+++ misc/files/test.c
@@ -6,6 +6,7 @@ int main() {
   a = 1;
 
   printf("a = %d, b = a++ which equals %d and now a = %d\n", a, ++a, a);
+  printf("a = %d, b = a++ which equals %d and now a = %d\n", a, --a, a);
 
   return 0;
 
