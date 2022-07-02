8552a8d6fd557396aa69da0e851e6d8c4d602d5c
Kody Wilson <kody.wilson@oracle.com>
2022-07-02 09:22:07 -0700

    drawing shapes with ncurses

---
+++ misc/ncurses/animations/shapes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+int main() {
+  int max_y, max_x, radius;
+
+  initscr();
+  start_color();
+
+  //(x – h)2 + (y – v)2 = r2
+  //x2 + y2 = r2
+
+  endwin();
+  return 0;
+}
