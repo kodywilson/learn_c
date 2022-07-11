e74b76cd9d2c78d71a04c2b38e8d0d39df842ab8
Kody Wilson <kodywilson@gmail.com>
2022-07-11 15:40:59 -0700

    load game from save after game over scene

--- game_dev/destinc/destinc.c
+++ game_dev/destinc/destinc.c
@@ -163,8 +163,10 @@ int main() {
     main_loop = town(game_text, select, stats, saves, &player);
     if (main_loop == 13) {
       game_over(game_text, player.name);
-      player.cur_hp = player.max_hp;     // restore health
-      player.cur_mana = player.max_mana; // restore mana
+      choice = choose_save(game_text, select, saves);
+      player = saved_games[choice];
+      //player.cur_hp = player.max_hp;     // restore health
+      //player.cur_mana = player.max_mana; // restore mana
       all_windows(stats_border, stats, game_text_border, game_text, select_border, select, input, max_x);
       refresh_stats(stats, &player); // update stats window
     }
