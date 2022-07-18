edc3c389e48e302a46b70340228c3adbefdca741
Kody Wilson <kody.wilson@oracle.com>
2022-07-18 16:26:59 -0700

    working on quests

--- game_dev/destinc/entities/quests.h
+++ game_dev/destinc/entities/quests.h
@@ -3,6 +3,7 @@
 // Quest Struct
 // players will have quest slots and these will track active quests
 // I need to figure out how to best store completed quests...perhaps each has a unique id
+// that way I can track in progress, not yet attempted, and completed
 typedef struct Quest {
   char name[64];      // Quest name
 } quest;
