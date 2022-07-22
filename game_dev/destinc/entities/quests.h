05aebdc67f78513a358232de3ef93e7f831b3284
Kody Wilson <kodywilson@gmail.com>
2022-07-21 19:55:32 -0700

    working on quests

--- game_dev/destinc/entities/quests.h
+++ game_dev/destinc/entities/quests.h
@@ -1,5 +1,7 @@
 // functions and data for quests
 
+// Quests will be obtained in the tavern and from other events and places
+
 // Quest Struct
 // players will have quest slots and these will track active quests
 // I need to figure out how to best store completed quests...perhaps each has a unique id
